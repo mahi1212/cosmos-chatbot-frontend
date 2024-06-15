abb3ca385569a8cd8efa63070f8af4610ae42563
mahi1212 <mahi121.mr@gmail.com>
2024-06-16 00:41:44 +0600

    email js added in contact form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,13 +1,45 @@
 import { InboxIcon, PhoneIcon } from "@heroicons/react/24/outline"
+import { useRef, useState } from "react"
 import { Helmet } from "react-helmet"
 import toast from "react-hot-toast"
 import { BsGithub } from "react-icons/bs"
 import { LiaLinkedin } from "react-icons/lia"
 import { Link } from "react-router-dom"
-
+import emailjs from '@emailjs/browser';
 
 
 const Contact: React.FC = () => {
+
+    const form = useRef<HTMLFormElement | null>(null);
+
+    const handleSendMail = (e: React.FormEvent) => {
+        e.preventDefault();
+        // validate form by checking if name, email and message is not empty
+        if (form.current) {
+            const formData = new FormData(form.current);
+            const name = formData.get('name') as string;
+            const email = formData.get('email') as string;
+            const message = formData.get('message') as string;
+            if (!name || !email || !message || name === '' || email === '' || message === '' || !email.includes('@') || name.trim() === '' || email.trim() === '' || message.trim() === '') {
+                toast.error('Please provide required data');
+                return;
+            }
+
+            if (form.current !== null) {
+                emailjs.sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
+                    .then((result) => {
+                        console.log(result.text);
+                    },  
+                        (error) => {
+                            console.log(error.text);
+                            toast.error('Something went wrong, please try again later');
+                        });
+            }
+            toast.success('Message sent successfully');
+            form.current.reset();
+        };
+    };
+
     return (
         <main className="container mx-auto px-4 py-12 md:px-6 lg:py-16">
             {/* for title of page using react Helmet*/}
@@ -72,26 +104,25 @@ const Contact: React.FC = () => {
                     </div>
                 </div>
                 <div className="space-y-6">
-                    <form className="grid gap-6">
+                    <form className="grid gap-6" ref={form} onSubmit={handleSendMail}>
                         <div className="grid gap-2">
-                            <label htmlFor="name">Name</label>
-                            <input id="name" placeholder="Enter your name" className="border-2 p-2 rounded-md outline-none" />
+                            <label htmlFor="name">Name*</label>
+                            <input id="name" name="name" placeholder="Enter your name" className="border-2 p-2 rounded-md outline-none" />
                         </div>
                         <div className="grid gap-2">
-                            <label htmlFor="email">Email</label>
-                            <input id="email" placeholder="Enter your email" type="email" className="border-2 p-2 rounded-md outline-none" />
+                            <label htmlFor="email">Email*</label>
+                            <input id="email" name="email" placeholder="Enter your email" type="email" className="border-2 p-2 rounded-md outline-none" />
                         </div>
                         <div className="grid gap-2">
-                            <label htmlFor="message">Message</label>
-                            <textarea id="message" placeholder="Enter your message" rows={5} className="border-2 p-2 rounded-md outline-none" />
+                            <label htmlFor="message">Message*</label>
+                            <textarea id="message" name="message" placeholder="Enter your message" rows={5} className="border-2 p-2 rounded-md outline-none" />
                         </div>
-                        <button className="w-full border-2 py-2 bg-slate-800 text-white rounded-md" type="submit"
-                            onClick={(e) => {
-                                e.preventDefault()
-                                toast.success('Use email to contact me.')
-                            }}
-                        >
-                            Send Message
+                        <button className="w-full border-2 py-2 bg-slate-800 text-white rounded-md flex items-center justify-center gap-2 hover:bg-slate-900 transition" type="submit">
+                            SEND MESSAGE
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                            </svg>
+
                         </button>
                     </form>
                 </div>
