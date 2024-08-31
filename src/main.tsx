32f518de4599984959f2bf22c1609b0dc79dfff7
mahi1212 <mahi121.mr@gmail.com>
2024-08-31 14:18:52 +0600

    added base url

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:5000/api/v1';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
