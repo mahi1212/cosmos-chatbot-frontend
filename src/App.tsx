ab24fd2ad0ca854fd200cbbb4f6bf4925bdb5377
mahi1212 <mahi121.mr@gmail.com>
2024-09-04 12:58:16 +0600

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import {
-  RouterProvider
-} from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { router } from "./routes/router"
 import { AuthProvider } from './context/AuthContent'
 import { Toaster } from 'react-hot-toast'
 
 function App() {
   return <AuthProvider>
-    <Toaster position='bottom-right' /> 
+    <Toaster position='bottom-right' />
     <RouterProvider router={router} />
   </AuthProvider>
 }
